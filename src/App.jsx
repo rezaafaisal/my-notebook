7ae88295b1160e1b106e6194aa9ea5314284385c
rezaafaisal <rezha.faizal567@gmail.com>
2023-06-25 15:39:18 +0800

    init add tailwind & react router

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <>
+      <p className='text-gray-100 bg-blue-500 block h-10 w-50'>Selamat datang sayang aokwaokwoak</p>
+    </>
+  )
+}
+
+export default App
